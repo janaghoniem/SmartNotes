304c29395b533bd9520fe7dff1cc9d0148dea783
janaghoniem <jana2208912@miuegypt.edu.eg>
2024-12-09 19:23:01 +0200

    path edit

--- app/Views/login.php
+++ app/Views/login.php
@@ -170,10 +170,10 @@ if (isset($_POST['email1'])) {
     </div>
 
     <!-- jQuery and Bootstrap Bundle JS -->
-    <script src="../assets/js/admin_form_validation.js"></script>
+    <script src="../../public/assets/js/admin_form_validation.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
-    <script src="../assets/js/login.js"></script>
+    <script src="../../public/assets/js/login.js"></script>
 </body>
 
 </html>
